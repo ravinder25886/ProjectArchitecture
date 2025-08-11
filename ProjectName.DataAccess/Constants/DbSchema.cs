2b269efd6af7cd5414ad416add86c6fe1688b460
Ravinder <ravinder25886@gmail.com>
2025-08-11 20:58:20 +0100

    fix: applied no updated for DbSchema

--- ProjectName.DataAccess/Constants/DbSchema.cs
+++ ProjectName.DataAccess/Constants/DbSchema.cs
@@ -7,7 +7,7 @@ namespace ProjectName.DataAccess.Constants;
 public static class DbSchema
 {
     //private const string Schema = "dbo";
-    private const string Schema = "";// In MYSQL database case we canot use any Schema
+    private const string _schema = "";// In MYSQL database case we canot use any Schema
     //public const string Schema = "public";// In PostgreSql database case we canot use any Schema
 
     private static DatabaseType _dbType = DatabaseType.SqlServer;
@@ -17,14 +17,14 @@ public static class DbSchema
         Enum.TryParse(dbSettings["Type"], out _dbType);
     }
     // Tables
-    public static string UserTable => DbIdentifierHelper.GetTable($"User", Schema, _dbType);
-    public static string CategoryTable => DbIdentifierHelper.GetTable($"Category", Schema, _dbType);
+    public static string UserTable => DbIdentifierHelper.GetTable($"User", _schema, _dbType);
+    public static string CategoryTable => DbIdentifierHelper.GetTable($"Category", _schema, _dbType);
 
     // Stored Procedures
-    public static string UserGetByIdProc => DbIdentifierHelper.GetProcedure($"User_GetById", Schema, _dbType);
-    public static string UserInsertProc => DbIdentifierHelper.GetProcedure($"User_Insert", Schema, _dbType);
-    public static string UserUpdateProc => DbIdentifierHelper.GetProcedure($"User_Update", Schema, _dbType);
-    public static string UserDeleteProc => DbIdentifierHelper.GetProcedure($"User_Delete", Schema, _dbType);
-    public static string UserGetAllProc => DbIdentifierHelper.GetProcedure($"User_GetAll", Schema, _dbType);
+    public static string UserGetByIdProc => DbIdentifierHelper.GetProcedure($"User_GetById", _schema, _dbType);
+    public static string UserInsertProc => DbIdentifierHelper.GetProcedure($"User_Insert", _schema, _dbType);
+    public static string UserUpdateProc => DbIdentifierHelper.GetProcedure($"User_Update", _schema, _dbType);
+    public static string UserDeleteProc => DbIdentifierHelper.GetProcedure($"User_Delete", _schema, _dbType);
+    public static string UserGetAllProc => DbIdentifierHelper.GetProcedure($"User_GetAll", _schema, _dbType);
     
 }
