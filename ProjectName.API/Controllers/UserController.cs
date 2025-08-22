65f90cf96c6847221284b72c9be8096c09905664
Ravinder <ravinder25886@gmail.com>
2025-08-22 13:55:56 +0100

    feat: removed dapper cotaext class now connection will be managed by DatabaseResolver

--- ProjectName.API/Controllers/UserController.cs
+++ ProjectName.API/Controllers/UserController.cs
@@ -69,4 +69,5 @@ public class UserRequest
     public string FullName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
 }
--- ProjectName.DataAccess/Account/Login/LoginRepository.cs
+++ ProjectName.DataAccess/Account/Login/LoginRepository.cs
@@ -1,24 +1,11 @@
-
-using System.Data;
-using Dapper;
 using ProjectName.Models.Account;
-using RS.Dapper.Utility.Connections;
 
 namespace ProjectName.DataAccess.Account.Login;
 // Implementation using Dapper or any DB access logic
-public class LoginRepository(DapperContext context) : ILoginRepository
+public class LoginRepository() : ILoginRepository
 {
-    private readonly DapperContext _context = context;
-
-    public async Task<LoginResponse> LoginAsync(LoginRequest loginRequest)
+    public Task<LoginResponse> LoginAsync(LoginRequest loginRequest)
     {
-        using var connection = _context.CreateConnection();
-        DynamicParameters parameters = new DynamicParameters();
-        parameters.Add("@UserName", loginRequest.UserName, DbType.String);
-        parameters.Add("@Password", loginRequest.Password, DbType.String);
-
-        var user = await connection.QueryFirstOrDefaultAsync<LoginResponse>("PROC_Name", parameters, commandType: CommandType.StoredProcedure);
-
-        return user ?? throw new UnauthorizedAccessException("Invalid credentials.");
+        throw new NotImplementedException();
     }
 }
